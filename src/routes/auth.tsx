10cc06b08cf82b91a7da1dd060b7cb52d9d56a91
Nora <norayousef56789@gmail.com>
2026-08-06 14:05:54 +0300

    fix redirect link

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -61,7 +61,7 @@ function AuthPage() {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}`,
+      redirectTo: "https://luxor-ai-new.vercel.app",
     },
   });
 
